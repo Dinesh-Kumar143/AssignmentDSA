6c5a5ff85d6609c81181966e9fb06e2ec4677ae0
Dinesh-Kumar143 <Dineshnarwani143@gmail.com>
2023-09-10 14:37:16 +0500

    Changes

--- TestCases-2/TestCases/Question 1/Q1.cpp
+++ TestCases-2/TestCases/Question 1/Q1.cpp
@@ -9,6 +9,7 @@ struct Node{
 };
 
 struct Node* head = NULL;
+static int n=0;
 
 void insert(int val){
     struct Node* node = (struct Node*) malloc(sizeof(struct Node));
@@ -18,6 +19,7 @@ void insert(int val){
     if (head == NULL){
         node->next = NULL;
         head = node;
+        n++;
         return;
     }
     struct Node* curr = head;
@@ -25,7 +27,8 @@ void insert(int val){
         curr = curr->next;
     }
     node->next = NULL;
-    curr->next = node;  
+    curr->next = node; 
+    n++; 
     return;
 }
 
@@ -38,6 +41,7 @@ void DELETE(){
         todelete = head;
         head = NULL;
         free(todelete);
+        n--;
         return;
     }
     struct Node* curr = head;
@@ -47,24 +51,38 @@ void DELETE(){
     todelete = curr->next;
     curr->next = NULL;
     free(todelete);
+    n--;
 }
+
 
 void GetTrump(){
     struct Node* fast = head;
     struct Node* slow = head;
-    if (head == NULL){
+    if (n == 0){
         return;
     }
-    else if (head->next == NULL){
-        cout<<head->data<<endl;
+    else if (n == 1){
+        cout<<slow->data;
         return;
     }
-    while(fast->next != NULL && fast != NULL){
-        fast = fast->next->next;
-        slow  = slow->next;
+    if ((n % 2) == 1){
+        while(fast->next != NULL && fast != NULL ){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        cout<<"Mid = "<<slow->data<<endl;
+        return ;
     }
-    cout<<slow->data<<endl;
+    else if((n % 2) == 0){
+        while(fast->next->next != NULL && fast != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        cout<<"Mid = "<<slow->data<<", "<<slow->next->data<<endl;
+        return ;
+    }
 }
+
 void display(){
     struct Node* curr = head;
 
@@ -74,11 +92,11 @@ void display(){
     }
 }
 int main(){
-    string file = "E:/BSCS-402-Data_Structure_And_Algorithim/Assignments DSA/Assignment 2/TestCases-2/TestCases/Question 1/01/Test01.txt";
+    // string file = "E:/BSCS-402-Data_Structure_And_Algorithim/Assignments DSA/Assignment 2/TestCases-2/TestCases/Question 1/01/Test01.txt";
     // string file = "E:/BSCS-402-Data_Structure_And_Algorithim/Assignments DSA/Assignment 2/TestCases-2/TestCases/Question 1/01/Test02.txt";
     // string file = "E:/BSCS-402-Data_Structure_And_Algorithim/Assignments DSA/Assignment 2/TestCases-2/TestCases/Question 1/01/Test03.txt";
     // string file = "E:/BSCS-402-Data_Structure_And_Algorithim/Assignments DSA/Assignment 2/TestCases-2/TestCases/Question 1/01/Test04.txt";
-    // string file = "E:/BSCS-402-Data_Structure_And_Algorithim/Assignments DSA/Assignment 2/TestCases-2/TestCases/Question 1/01/Test05.txt";
+    string file = "E:/BSCS-402-Data_Structure_And_Algorithim/Assignments DSA/Assignment 2/TestCases-2/TestCases/Question 1/01/Test05.txt";
     string first_read;
     int val;
     ifstream in(file);
@@ -95,10 +113,11 @@ int main(){
         else if( first_read == "END"){
             display();
             cout<<"NULL"<<endl;
+            GetTrump();
+            cout<<endl;
         }
         
     }
-    GetTrump();
     in.close();
     return 0;
 }
